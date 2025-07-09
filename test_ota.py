7d78263d332b01c5becdf639019d0aa1e7d7b45d
Indra Kurniawan <indranaftena@gmail.com>
2025-07-09 13:57:14 +0700

    mencoba di github indra

---
+++ test_ota.py
@@ -0,0 +1,11 @@
+from machine import Pin
+from time import sleep
+p5 = Pin(5, Pin.OUT)
+
+def loop():
+  p5.value(0)
+  sleep(0.2)
+  p5.value(1)
+  sleep(0.2)
+  print("Hallo poliwangi")
+  print("Dari github indra")
