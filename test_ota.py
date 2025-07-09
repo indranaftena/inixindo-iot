3ded5e9cd0b0eeaa624552b4245656e892710863
Indra Kurniawan <indranaftena@gmail.com>
2025-07-09 14:11:45 +0700

    yah gitu

--- test_ota.py
+++ test_ota.py
@@ -4,8 +4,9 @@ p5 = Pin(5, Pin.OUT)
 
 def loop():
   p5.value(0)
-  sleep(0.2)
+  sleep(0.4)
   p5.value(1)
-  sleep(0.2)
+  sleep(0.4)
   print("Hallo poliwangi")
   print("Dari github indra")
+  print("Hello dari Pak Arif Fahmi")
